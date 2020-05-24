0e3f9c54bba2b65a9745fb952113b6df7fb4265c
Mustafa <mustafahafidi96@gmail.com>
2020-05-24 11:49:17 +0200

    [QQ] handling Parameters attempt using reify to get type

--- src/TH/Main.hs
+++ src/TH/Main.hs
@@ -11,12 +11,44 @@ import Language.Haskell.Meta.Parse
 {-======================================================
                         entrypoint String
 =======================================================-}
-generateProof :: String -> Q [Dec]
-generateProof exp = case parseExp exp of
+generateProofFromExp :: String -> Q [Dec]
+generateProofFromExp exp = case parseExp exp of
                             Left err -> fail $ "[qc-to-lh] error: The given expression cannot be parsed"
                             Right parsedExp -> do
                                 pName <- newName "proof"
                                 lhDec   <- (lqDec $ show (pName) ++ " :: {v:()| " ++ exp ++"}")
                                 bodyDec <- [d| $(varP $ mkName (show pName)) = toProof $(pure parsedExp) |]
                                 return $ lhDec ++ bodyDec
-                        
+
+
+
+{-======================================================
+                        entrypoint multiparameter
+=======================================================-}
+generateProofFromVar :: Q (Maybe Name) -> Q [Dec]
+generateProofFromVar varName = 
+        do
+            mName <- varName
+            case mName of
+                Just name -> do
+                    info <- reify name
+                    case info of
+                        VarI nm tp mdecl -> fail $ pprint $ tp
+
+                        _                -> fail $ "[qc-to-lh] error: given binder doesn't represent a value variable (" ++ (show name) ++")"
+                Nothing -> fail "[qc-to-lh] Cannot find given property name" 
+    
+    
+            {- case parseExp exp of
+                            Left err -> fail $ "[qc-to-lh] error: The given expression cannot be parsed"
+                            Right parsedExp -> do
+                                pName <- newName "proof"
+                                lhDec   <- (lqDec $ show (pName) ++ " :: {v:()| " ++ exp ++"}")
+                                bodyDec <- [d| $(varP $ mkName (show pName)) = toProof $(pure parsedExp) |]
+                                return $ lhDec ++ bodyDec
+
+
+ -}
+-- >>> parseDecs "testProp1 :: Eq a => a -> Bool"
+-- Right [SigD testProp1 (ForallT [] [AppT (ConT Eq) (VarT a)] (AppT (AppT ArrowT (VarT a)) (ConT Bool)))]
+--
--- src/TH/Test.hs
+++ src/TH/Test.hs
@@ -7,6 +7,7 @@ import TH.TestProps
 
 import Language.Haskell.Liquid.ProofCombinators
 import Language.Haskell.TH
+import LiquidHaskell
 
 -- {-@ LIQUID "--diff" @-}
 {-@ LIQUID "--reflection" @-}
@@ -24,7 +25,7 @@ main = putStrLn $(do
 -- VarI TH.TestProps.ttt (ConT GHC.Types.Int) Nothing
 --
 ----------------------------------------------------------------
--- $(generateProof [| True == True |]) 
+-- $(generateProofFromExp [| True == True |]) 
 -- this comes as an Q Exp, I need a string to give to lhQQ, showing it gives
 -- InfixE (Just (ConE GHC.Types.True)) (VarE GHC.Classes.==) (Just (ConE GHC.Types.True))
 
@@ -33,9 +34,37 @@ main = putStrLn $(do
 {-@ reflect testProp @-}
 testProp = True == True
 
-$(generateProof "True == True")
-$(generateProof "testProp")
-$(generateProof "False")
--- $(generateProof2 "var = 2")
+-- $(generateProofFromExp "True == True")
+-- $(generateProofFromExp "testProp")
+-- $(generateProofFromExp "False")
+-- $(generateProofFromExp "var = 2")
 
+----------------------------------------------------------------
+
+----------------------------------------------------------------
+-- property with parameters
+{-@ reflect testProp @-}
+-- [lq| testProp1 :: Eq a => a -> {v:Bool | True} |]
+testProp1 :: Eq a => a -> Bool -> Bool
+testProp1 x y = x == x
+
+$(generateProofFromVar $ lookupValueName "testProp1")
+
+
+-- $(generateProofFromVar $ lookupValueName "asddddd") --no
+-- data TestType = Asd
+-- $(generateProofFromVar $ lookupValueName "Asd") --no
+
+
+
+
+main1 :: IO ()
+main1 = putStrLn $(do
+        Just nm <- lookupValueName "testProp1"
+        info <- reify nm
+        stringE . show $ info)  
+
+-- >>> main1
+-- VarI TH.Test.testProp1 (ForallT [KindedTV a_6989586621679468335 StarT] [AppT (ConT GHC.Classes.Eq) (VarT a_6989586621679468335)] (AppT (AppT ArrowT (VarT a_6989586621679468335)) (AppT (AppT ArrowT (ConT GHC.Types.Bool)) (ConT GHC.Types.Bool)))) Nothing
+--
 ----------------------------------------------------------------
