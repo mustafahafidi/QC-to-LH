f9e155a50debe86b7943675e465ec2b802bde872
Mustafa <mustafahafidi96@gmail.com>
2020-05-25 09:04:16 +0200

    [QQ] Handling multiple parameters, pattern guards, multiple clauses

--- src/TH/Main.hs
+++ src/TH/Main.hs
@@ -6,6 +6,10 @@ import Language.Haskell.TH
 import Language.Haskell.Liquid.UX.QuasiQuoter
 import Language.Haskell.Liquid.ProofCombinators
 import Language.Haskell.Meta.Parse
+import Control.Monad
+
+import Data.List
+import Data.Strings
 
 
 {-======================================================
@@ -13,7 +17,7 @@ import Language.Haskell.Meta.Parse
 =======================================================-}
 generateProofFromExp :: String -> Q [Dec]
 generateProofFromExp exp = case parseExp exp of
-                            Left err -> fail $ "[qc-to-lh] error: The given expression cannot be parsed"
+                            Left err -> reportErrorToUser $ "The given expression cannot be parsed"
                             Right parsedExp -> do
                                 pName <- newName "proof"
                                 lhDec   <- (lqDec $ show (pName) ++ " :: {v:()| " ++ exp ++"}")
@@ -23,7 +27,7 @@ generateProofFromExp exp = case parseExp exp of
 
 
 {-======================================================
-                        entrypoint multiparameter
+                        from var
 =======================================================-}
 generateProofFromVar :: Q (Maybe Name) -> Q [Dec]
 generateProofFromVar varName = 
@@ -39,16 +43,112 @@ generateProofFromVar varName =
                 Nothing -> fail "[qc-to-lh] Cannot find given property name" 
     
     
-            {- case parseExp exp of
-                            Left err -> fail $ "[qc-to-lh] error: The given expression cannot be parsed"
-                            Right parsedExp -> do
-                                pName <- newName "proof"
-                                lhDec   <- (lqDec $ show (pName) ++ " :: {v:()| " ++ exp ++"}")
-                                bodyDec <- [d| $(varP $ mkName (show pName)) = toProof $(pure parsedExp) |]
-                                return $ lhDec ++ bodyDec
+{-======================================================
+                    From decl
+=======================================================-}
+generateProofFromDecl :: String -> Q [Dec]
+generateProofFromDecl decs = 
+            case parseDecs decs of
+                Left err -> reportErrorToUser $ "The given declaration cannot be parsed: " ++ decs++err
+                Right parsedDecls -> do
+                    -- checking declarations
+                    when (length parsedDecls < 2) (reportErrorToUser $ "Please provide both type signature and body of the function.")
+                    -- getting the signature
+                    let (sd:(bodyDs@(bd:bs))) = parsedDecls
+                    let (SigD nm sigType) = sd
+                    -- reportError $ show $ bodyDs
+                    -- generate name for the proof
+                    let proofName = show nm ++"_proof"
+                    -- let sigTypes = strSplitAll "->" $ pprint (SigD (mkName proofName) sigType)
+                    -- check that it returns a boolean
+                    case isSuffixOf "Bool" (pprint sd) of
+                        False -> reportErrorToUser "The given declaration must return a boolean to be transformed to a LiquidHaskell proof"
+                       
+                        True  -> do
+                          -- (SigD (mkName proofName) sigType)
+                                -- add parameters to signature
+                                    (typeWP, pr) <- addParamsToType sigType []
+                                    -- reportError $ show typeWP ++ show pr
+                                    let sigTypesWP = strSplitAll "->" $ pprint typeWP
+
+                                -- replace return type with refinement type
+                                    let (fr,_) = splitAt (length sigTypesWP - 1) sigTypesWP
+                                    let (params,_) = splitAt (length pr - 1) pr
+                                    let finalRefType = (show $ mkName proofName) ++ " :: " ++ (strJoin " -> " $
+                                                      fr ++ ["{v:Proof | "++show nm++" "++(strJoin " " (map (\p->show p) params))++"}"])
+                                    -- reportErrorToUser $ finalRefType
+                                    lhDec <- (lqDec finalRefType)
+                                -- generate the body
+                                    -- add the ***QED to the body for each clause
+
+                                    -- (FunD nm clss)
+                                    let finalBody = case bd of
+                                                      FunD _ clss -> 
+                                                                let proofClss = map  (\(Clause pns body decs) -> (Clause pns (wrapBodyWithProof body) decs)) clss
+                                                                in FunD (mkName proofName) proofClss
+                                                      ValD _ body decs  -> ValD (VarP (mkName proofName)) (wrapBodyWithProof body) decs
+                                    
+
+                                                        
+                                    return $ lhDec ++ [finalBody]
+                   
+
+findSignature :: [Dec] -> Q Dec
+findSignature decs =  case maybeFound of
+                            Nothing -> reportErrorToUser "Cannot find the signature of the function declaration given"
+                            Just dec -> return dec
+                where maybeFound = find (\d -> case d of
+                                                (SigD _ _) -> True
+                                                _ -> False) decs
+
+-- |Given a signature adds parameters and returns them
+-- addParamsToSignature :: Dec -> Q (Dec, [Name])
+-- addParamsToSignature (SigD nm sigType) =  do (typeWithParams, params) <-  addParamsToType sigType []
+--                                              return ((SigD  nm typeWithParams), params)
+--       where
+addParamsToType :: Type -> [Name] -> Q (Type, [Name])
+addParamsToType (ForallT tvb ctx tp) acc = do (rect, params) <- addParamsToType tp acc
+                                              return ((ForallT tvb ctx rect), acc++params)
+
+addParamsToType (AppT t1 t2) acc = do (rect1,p1) <- addParamsToType t1 []
+                                      (rect2,p2) <- addParamsToType t2 []
+                                      return ((AppT rect1 rect2),acc++p1++p2)
+
+addParamsToType (VarT nm) acc = do nName <- newName "p"
+                                   return ((VarT (mkName $ show nName++":"++show nm)), [nName])
+addParamsToType (ConT nm) acc = do nName <- newName "p"
+                                   return ((ConT (mkName $ show nName++":"++show nm)), [nName])
+addParamsToType v acc = return (v, acc)
+-- addParamsToType v        = reportErrorToUser $ "Cannot parse the signature to add LH parameters in it:"++" don't konw how to treat " ++ (show v)
 
 
+wrapBodyWithProof :: Body -> Body
+wrapBodyWithProof oldBody@(NormalB bodyExp) = case parseExp $ "(" ++ pprint bodyExp ++ ")***QED" of
+                                        Right newBody -> NormalB newBody
+                                        Left err -> oldBody
+
+wrapBodyWithProof (GuardedB gds) = GuardedB guards
+                                            where
+                                                transformBody oldBody = 
+                                                  case parseExp $ "(" ++ pprint oldBody ++ ")***QED" of
+                                                    Right newBody ->  newBody
+                                                    Left err -> oldBody
+                                                    
+                                                guards = map (\(g, bodyExp) -> (g, transformBody bodyExp)) gds
+
+
+reportErrorToUser:: MonadFail m => String -> m a
+reportErrorToUser message = fail $ "[qc-to-lh] error: "++ message
+
+
+
+{- 
+
+Current usage:
+  - Parsing signature supports only normal currying (no functions as arguments (e.g. (a->b) -> c))
+  - To use local PLE it's possible to use the propertyName_proof
  -}
--- >>> parseDecs "testProp1 :: Eq a => a -> Bool"
--- Right [SigD testProp1 (ForallT [] [AppT (ConT Eq) (VarT a)] (AppT (AppT ArrowT (VarT a)) (ConT Bool)))]
+
+-- >>> parseExp "  (case x of\n True -> True\n _ -> False)==True"
+-- Right (UInfixE (ParensE (CaseE (VarE x) [Match (ConP True []) (NormalB (ConE True)) [],Match WildP (NormalB (ConE False)) []])) (VarE ==) (ConE True))
 --
--- src/TH/Test.hs
+++ src/TH/Test.hs
@@ -8,25 +8,30 @@ import TH.TestProps
 import Language.Haskell.Liquid.ProofCombinators
 import Language.Haskell.TH
 import LiquidHaskell
+import NeatInterpolation
+import Data.Text
 
 -- {-@ LIQUID "--diff" @-}
 {-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple-local" @-}
 
 
 ---------------------------------------------------------------
 -- Can't get the body of `ttt` because not implemented by GHC
 -- http://hackage.haskell.org/package/template-haskell-2.16.0.0/docs/Language-Haskell-TH.html#t:Info
+{- 
 main :: IO ()
 main = putStrLn $(do
         Just nm <- lookupValueName "ttt"
         info <- reify nm
-        stringE . show $ info)  
+        stringE . show $ info)   
+-}
 -- >>> main
 -- VarI TH.TestProps.ttt (ConT GHC.Types.Int) Nothing
 --
 ----------------------------------------------------------------
 -- $(generateProofFromExp [| True == True |]) 
--- this comes as an Q Exp, I need a string to give to lhQQ, showing it gives
+-- this comes as an Q Exp, I need a string to give to lh QQ, showing it gives
 -- InfixE (Just (ConE GHC.Types.True)) (VarE GHC.Classes.==) (Just (ConE GHC.Types.True))
 
 ----------------------------------------------------------------
@@ -42,29 +47,136 @@ testProp = True == True
 ----------------------------------------------------------------
 
 ----------------------------------------------------------------
--- property with parameters
-{-@ reflect testProp @-}
--- [lq| testProp1 :: Eq a => a -> {v:Bool | True} |]
-testProp1 :: Eq a => a -> Bool -> Bool
-testProp1 x y = x == x
+-- Trying to lookup the info about a binder
 
-$(generateProofFromVar $ lookupValueName "testProp1")
-
-
+-- $(generateProofFromVar $ lookupValueName "testProp1")
 -- $(generateProofFromVar $ lookupValueName "asddddd") --no
 -- data TestType = Asd
 -- $(generateProofFromVar $ lookupValueName "Asd") --no
 
+----------------------------------------------------------------
+-- Going the string way
+-----------
+{-@ reflect asddd @-}
+asddd ::   Bool
+asddd  =  True
+
+generateProofFromDecl  "asddd :: Bool\n\
+                        \asddd = True"
+-----------
+-- Multiple parameters test
+-----------
+{-@ reflect testProp1 @-}
+testProp1 ::  Bool -> Bool -> Bool
+testProp1 x y = True
+
+{-@ ple testProp1_proof  @-}
+$(generateProofFromDecl $ "testProp1 ::  Bool -> Bool -> Bool\n\
+                          \testProp1 x y = True")
+
+-----------
+-- Multiple parameters using multiline QQ
+-----------
+{-@ reflect testProp2 @-}
+testProp2 ::  Bool -> Bool -> Bool
+testProp2 x y = (case x of
+                    True -> y
+                    _    -> False) == (x && y)
+
+{-@ ple testProp2_proof  @-}
+generateProofFromDecl $ unpack [text| 
+                                        testProp2 ::  Bool -> Bool -> Bool
+                                        testProp2 x y = (case x of
+                                                            True -> y
+                                                            _    -> False) == (x && y)
+                               |]
+
+-----------
+-- Multiple declarations
+-----------
+{-@ reflect testProp3 @-}
+testProp3 ::  Bool -> Bool
+testProp3 True =  True
+testProp3 False = True
+
+{-@ ple testProp3_proof  @-}
+generateProofFromDecl $ unpack [text| 
+                                testProp3 :: Bool -> Bool
+                                testProp3 True =  True
+                                testProp3 False = True
+                               |]
+ 
+
+-----------
+-- Multiple pattern guards
+-----------
+{-@ reflect testProp4 @-}
+testProp4 ::  Int -> Bool
+testProp4 x
+    | x==0 = True
+    | otherwise = False
+
+{-@ ple testProp4_proof  @-}
+generateProofFromDecl $ unpack [text| 
+
+testProp4 :: Int -> Bool
+testProp4 x
+        | x==0 = True
+        | otherwise = False 
+                               |]
+
+-----------
+-- Not supported yet
+-----------
+{-@ reflect testFunctPar @-}
+testFunctPar ::  (Bool -> Bool) -> Bool
+testFunctPar f = f True == f True
+
+{-@ ple testFunctPar_proof  @-}
+generateProofFromDecl $ unpack [text| 
+
+testFunctPar ::  (Bool -> Bool) -> Bool
+testFunctPar f = f True == f True
+
+                               |]
+{- 
+Current usage:
+  - Parsing signature supports only normal currying (no functions as arguments (e.g. (a->b) -> c))
+  - It's possible to refer to the proof name as propertyName_proof. To use local PLE it's possible to use the propertyName_proof
+-}
 
 
 
+-- {-@ ple testProp1_proof  @-}
+-- testlq x = [lq| testProp1_proof :: x:Bool -> y:Bool -> {v:Proof| $x } |]
+-- testProp1_proof x y = (True) *** QED
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------
+{- 
 main1 :: IO ()
 main1 = putStrLn $(do
         Just nm <- lookupValueName "testProp1"
         info <- reify nm
-        stringE . show $ info)  
+        stringE . show $ info)
 
--- >>> main1
--- VarI TH.Test.testProp1 (ForallT [KindedTV a_6989586621679468335 StarT] [AppT (ConT GHC.Classes.Eq) (VarT a_6989586621679468335)] (AppT (AppT ArrowT (VarT a_6989586621679468335)) (AppT (AppT ArrowT (ConT GHC.Types.Bool)) (ConT GHC.Types.Bool)))) Nothing
+main2 :: IO ()
+main2 = putStrLn $(do
+            decs <- [d| asd :: Int
+                        asd = 3 |]
+            stringE (pprint decs)
+        ) -}
+-- >>> main2
+-- 2
 --
 ----------------------------------------------------------------
